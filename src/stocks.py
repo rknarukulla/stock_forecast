22249b0881c72caa5f0f6f28bf42ff4ab3a4da45
ravinarukulla <n.ravikumar@live.com>
2024-04-07 12:24:44 +0530

    refactored code

--- src/stocks.py
+++ src/stocks.py
@@ -1,16 +1,29 @@
+import datetime
 from datetime import timedelta
 
 import pandas as pd
 import ta
-import yfinance as yf
 import xgboost as xgb
-from sklearn.metrics import mean_squared_error
+import yfinance as yf
+
+PARAMS: dict = {
+    "max_depth": 3,
+    "eta": 1,
+    "objective": "reg:squarederror",
+    "eval_metric": "rmse",
+    "n_estimators": 100,
+    "random_state": 42,
+}
 
 
 #
 
 
-def download_stocks(stock_codes, start_date, end_date):
+def download_stocks(
+    stock_codes: list[str],
+    start_date: datetime.datetime,
+    end_date: datetime.datetime,
+):
     """"""
     # Fetch the stock data
     stock_data = {}
@@ -23,7 +36,14 @@ def download_stocks(stock_codes, start_date, end_date):
 def get_tech_features(data):
     # Add all ta features filling nans values
     return ta.add_all_ta_features(
-        data, open="Open", high="High", low="Low", close="Close", volume="Volume", fillna=True)
+        data,
+        open="Open",
+        high="High",
+        low="Low",
+        close="Close",
+        volume="Volume",
+        fillna=True,
+    )
 
 
 def get_lag_features(data, feature=None, lags=None):
@@ -35,30 +55,59 @@ def get_lag_features(data, feature=None, lags=None):
     count = data[feature]
 
     lag_features = [count.shift(x).rename(f"{feature}_lag_{x}d") for x in lags]
-    rolling_mean_features = [count.shift(1).rolling(x).mean().rename(f"{feature}_rolling_mean_{x}d") for x in lags]
+    rolling_mean_features = [
+        count.shift(1).rolling(x).mean().rename(f"{feature}_rolling_mean_{x}d")
+        for x in lags
+    ]
 
-    lagged_df = pd.concat([count, *lag_features, *rolling_mean_features], axis="columns")
+    lagged_df = pd.concat(
+        [count, *lag_features, *rolling_mean_features], axis="columns"
+    )
 
-    return lagged_df.drop(feature, axis=1, errors='ignore')
+    return lagged_df.drop(feature, axis=1, errors="ignore")
 
 
 def get_target(data, target_feature=None, future_days=1):
     # Calculate lagged features
 
     target_col = data[target_feature]
-    return target_col.shift(-future_days).rename(f"{target_feature}_next_{future_days}d")
+    return target_col.shift(-future_days).rename(
+        f"{target_feature}_next_{future_days}d"
+    )
 
 
-def build_model(X, y):
-    xgb_model = xgb.XGBRegressor(objective="reg:linear", random_state=42)
-    xgb_model.fit(X, y)
-    y_pred = pd.DataFrame(xgb_model.predict(X), index=X.index)
-    print(f"mean Square error in train data: {mean_squared_error(y, y_pred)}", )
+def build_model(X, y, validation_days=7):
+    x_train = X.iloc[:-validation_days]
+    y_train = y.iloc[:-validation_days]
+
+    x_val = X.iloc[-validation_days:]
+    y_val = y.iloc[-validation_days:]
+
+    drain = xgb.DMatrix(x_train, label=y_train)
+    deval = xgb.DMatrix(x_val, label=y_val)
+
+    # Build model
+    eval_list = [(drain, "train"), (deval, "test")]
+    xgb_model = xgb.train(PARAMS, drain, num_boost_round=10, evals=eval_list)
+
+    # xgb_model = xgb.XGBRegressor(objective="reg:linear", random_state=42)
+    # xgb_model.fit(X, y)
+    # y_pred = pd.DataFrame(xgb_model.predict(X), index=X.index)
+    # print(f"mean Square error in train data: {mean_squared_error(y, y_pred)}", )
 
     return xgb_model
 
 
-def build_n_forecast(stock_names):
+def forecast_stocks(x, model, model_name="xgboost"):
+    if model_name == "xgboost":
+        return model.predict(xgb.DMatrix(x))
+    else:
+        raise Exception("Invalid model")
+
+
+def build_n_forecast(
+    stock_names, date_column="Date", future_days=1, validation_days=7
+):
     all_predictions = pd.DataFrame()
     for stock_name in stock_names:
         print("Building model for ", stock_name)
@@ -66,32 +115,50 @@ def build_n_forecast(stock_names):
         target_path = f"target_{stock_name}.csv"
         test_real_path = f"features_{stock_name}_test.csv"
 
-        X = pd.read_csv(features_path).set_index("Date")
-        y = pd.read_csv(target_path).set_index("Date")
-        test_real = pd.read_csv(test_real_path).set_index("Date")
-        xgb_model = build_model(X, y)
+        X = pd.read_csv(features_path).set_index(date_column)
+        y = pd.read_csv(target_path).set_index(date_column)
 
-        y_real = pd.DataFrame(xgb_model.predict(test_real), index=test_real.index, columns=["forecast"]).reset_index()
+        test_real = pd.read_csv(test_real_path).set_index(date_column)
+        model = build_model(X, y, validation_days)
+
+        forecast = forecast_stocks(test_real, model, model_name="xgboost")
+        y_real = pd.DataFrame(
+            forecast, index=test_real.index, columns=["forecast"]
+        ).reset_index()
         y_real["Stock"] = stock_name
-        y_real["Forecast_period"] = pd.to_datetime(y_real["Date"]) + timedelta(days=1)
+        y_real["Forecast_period"] = pd.to_datetime(
+            y_real[date_column]
+        ) + timedelta(days=future_days)
         if stock_name == stock_names[0]:
             all_predictions = y_real
         else:
-            all_predictions = pd.concat([all_predictions, y_real], axis='index')
+            all_predictions = pd.concat([all_predictions, y_real], axis="index")
     return all_predictions
 
 
-def generate_all_features(stock_codes, start_date=None, end_date=None, test_days=1, get_test_data=False):
+def generate_all_features(
+    stock_codes,
+    start_date=None,
+    end_date=None,
+    test_days=1,
+    get_test_data=False,
+):
     y_test = pd.DataFrame()
     for stock_name in stock_codes:
         print("generating features for ", stock_name)
         stock_data = download_stocks([stock_name], start_date, end_date)
         data = get_tech_features(stock_data[stock_name])
-        lag_features = get_lag_features(data, feature='Close').drop('Close', axis=1, errors='ignore').fillna(0)
-        data_features = pd.concat([data, lag_features], axis='columns')
-        y = get_target(data_features, target_feature='Close')
+        lag_features = (
+            get_lag_features(data, feature="Close")
+            .drop("Close", axis=1, errors="ignore")
+            .fillna(0)
+        )
+        data_features = pd.concat([data, lag_features], axis="columns")
+        y = get_target(data_features, target_feature="Close")
         ind_features = data_features.drop(data_features.tail(test_days).index)
-        data_future = data_features[data_features.index.isin(data_features.tail(test_days).index)]
+        data_future = data_features[
+            data_features.index.isin(data_features.tail(test_days).index)
+        ]
         if get_test_data:
             y_test = y.tail(test_days)
         y = y.drop(y.tail(test_days).index)
